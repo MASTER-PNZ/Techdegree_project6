1d43668c8cbdddb5c032f4248486a9ecc75897e8
MASTER-PNZ <paulkeesling@gmail.com>
2019-02-12 17:06:44 -0500

    added dependencies, installed modules

---
+++ scraper.js
@@ -0,0 +1,21 @@
+// Package Requirements
+const fs = require("fs");
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+const json2csv = require('json2csv');
+
+// Website Entry url
+url = "http://shirts4mike.com/";
+// Date Format Programming
+const date = new Date();
+// Data file conditional/creation
+
+// Requesting Data and arraging html elements
+
+// Function to display scraped info
+
+// Function to create csv file
+
+// Error Log handler
+
+//
