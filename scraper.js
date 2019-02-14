883a4429c5f64c9f8d5dd54f83bda8aa8c7ab9d4
MASTER-PNZ <paulkeesling@gmail.com>
2019-02-14 14:53:40 -0500

    Making module skeleton for Promises and errors
    Promise and error handling frameworks. hopefully able to plug in the html with cheerio tomorrow.

--- scraper.js
+++ scraper.js
@@ -5,7 +5,7 @@ const cheerio = require('cheerio');
 const json2csv = require('json2csv');
 
 // Website Entry url
-url = "http://shirts4mike.com/";
+urlMain = "http://shirts4mike.com/";
 
 // Date Format Programming
 const date = new Date();
@@ -21,6 +21,7 @@ if (day < 10) {
 }
 
 let csvToday = `${year}-${month}-${day}`;
+// Gets the current time in a local, readable, format
 let currentTime = date.toLocaleTimeString();
 console.log(date);
 console.log(csvToday);
@@ -34,24 +35,37 @@ let dataFolder = './data';
 try {
   fs.accessSync(dataFolder);
 } catch (e) {
-  fs.mkdirSync()dataFolder;
+  fs.mkdirSync(dataFolder);
 }
 
 // Requesting Data and arraging html elements
+rp(urlMain)
+    .then(function (htmlString) {
+        // Process html...
+    })
+    .catch(function (err) {
+        // Crawling failed...
+    });
 
 // Function to display scraped info
 
 // Function to create csv file
-// const json2csv = require('json2csv').parse;
-// const fields = ['field1', 'field2', 'field3'];
-// const opts = { fields };
-//
-// try {
-//   const csv = json2csv(myData, opts);
-//   console.log(csv);
-// } catch (err) {
-//   console.error(err);
-// }
-// Error Log handler
+const json2csv = require('json2csv').parse;
+const fields = ['Title', 'Price', 'ImageURL', 'URL', 'Time'];
+const opts = { fields };
 
-//
+try {
+  const csv = json2csv(shritvariable, opts);
+  fs.writeFile(`./data/${csvToday}.csv`, csv, (err) => {
+
+
+    console.log("Website Sucessfully Crawled!");
+  });
+} catch (err) {
+  console.error(err);
+}
+// Error Log handler
+fs.appendFile("scraper-error.log", `[${date}] ${error}\n`, (error) => {
+  if (error) {
+    throw error;
+  }
